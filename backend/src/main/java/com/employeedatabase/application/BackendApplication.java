80805c12a3bb8346e473b140cef8b1cc84dfb5cd
ztlee95 <zhitao.lee@outlook.com>
2022-06-08 23:28:27 +0800

    minor naming refactoring and addition of model entity Employee

--- backend/src/main/java/com/employeedatabase/application/BackendApplication.java
+++ backend/src/main/java/com/employeedatabase/application/BackendApplication.java
@@ -1,4 +1,4 @@
-package com.employeedatabase.backend;
+package com.employeedatabase.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/com/employeedatabase/application/BackendApplicationTests.java
+++ backend/src/test/java/com/employeedatabase/application/BackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.employeedatabase.backend;
+package com.employeedatabase.application;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
