34401d86a1beab5a4dc9625b9c021853bc6f8b0d
ztlee95 <zhitao.lee@outlook.com>
2022-06-08 23:55:52 +0800

    added employee controller and get employees api

---
+++ backend/src/main/java/com/employeedatabase/controller/EmployeeController.java
@@ -0,0 +1,24 @@
+package com.employeedatabase.controller;
+
+import com.employeedatabase.model.Employee;
+import com.employeedatabase.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    // Get all employees
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+}
