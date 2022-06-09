dfcaa47883b76a9530256bf3361297cb3e0d9925
ztlee95 <zhitao.lee@outlook.com>
2022-06-10 01:56:08 +0800

    fetch api from backend and displayed in react using useState and useEffect hooks

--- frontend/src/components/ListEmployeeComponent.jsx
+++ frontend/src/components/ListEmployeeComponent.jsx
@@ -1,36 +1,46 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 const ListEmployeeComponent = () => {
-  
-    const [employees, setEmployees] = useState(null)
+    
+    const [employees, setEmployees] = useState([])
 
+    useEffect(()=>{
+        getEmployees()
+    },[])
+
+    const getEmployees = async () => {
+        const res = await fetch("http://localhost:8080/api/v1/employees")
+        const data = await res.json() 
+        setEmployees(data)
+    }
+    
     return (
-        <div>
-        <h2 className="text-center">Employees List</h2>
-        <div className="row">
-            <table className="table table-striped table-bordered">
-                <thead>
-                    <tr>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Email</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                {/* <tbody>
-                    {
-                        employees?.map(employee => 
-                            <tr key={employee.id}>
-                                <td>{employee.firstName}</td>
-                                <td>{employee.lastName}</td>
-                                <td>{employee.email}</td>
-                            </tr>
-                        )
-                    }
-                </tbody> */}
-            </table>
-        </div>
-        </div>
+        <>
+            <h2 className="text-center">Employees List</h2>
+            <div className="row">
+                <table className="table table-striped table-bordered">
+                    <thead>
+                        <tr>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Email</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            employees?.map((employee) => (
+                                <tr key={employee.id}>
+                                    <td>{employee.firstName}</td>
+                                    <td>{employee.lastName}</td>
+                                    <td>{employee.email}</td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+            </div>
+        </>
     )
 }
 
