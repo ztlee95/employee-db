43c4fd160868a4bdac1121608b34a27799726677
ztlee95 <zhitao.lee@outlook.com>
2022-06-10 02:15:50 +0800

    created header and footer component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import ListEmployeeComponent from './components/ListEmployeeComponent';
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
 
 function App() {
   return (
-    <div className="container">
+    <div>
+      <HeaderComponent/>
       <ListEmployeeComponent/>
+      <FooterComponent/>
     </div>
   );
 }
---
+++ frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div className="footer">
+      <span className="text-muted">All Rights Reserved 2022 @ztlee95</span>
+    </div>
+  )
+}
+
+export default FooterComponent
