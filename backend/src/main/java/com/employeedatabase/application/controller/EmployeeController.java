cb6acb7ed36a8ffc73b4d9bc6f13437f2f4360d0
ztlee95 <zhitao.lee@outlook.com>
2022-06-11 22:44:20 +0800

    added create employee post api

--- backend/src/main/java/com/employeedatabase/application/controller/EmployeeController.java
+++ backend/src/main/java/com/employeedatabase/application/controller/EmployeeController.java
@@ -3,12 +3,11 @@ package com.employeedatabase.application.controller;
 import com.employeedatabase.application.model.Employee;
 import com.employeedatabase.application.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins="http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1")
 public class EmployeeController {
@@ -20,5 +19,11 @@ public class EmployeeController {
     @GetMapping("/employees")
     public List<Employee> getAllEmployees(){
         return employeeRepository.findAll();
+    }
+
+    // Add employee
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
     }
 }
